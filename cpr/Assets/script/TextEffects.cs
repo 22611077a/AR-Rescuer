a07eb31eb347f8e78cef3bfe9080e35f623abb7b
22611077a <114436235+22611077a@users.noreply.github.com>
2023-09-02 19:59:38 +0800

    scene0

---
+++ cpr/Assets/script/TextEffects.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Txexteffects : MonoBehaviour
+{
+    string str;
+    Text tex;
+    int i = 0;
+    int index = 0;
+    string str1 = "";
+    bool ison = true;
+
+    void Start()
+    {
+        tex = GetComponent<Text>();
+        str = tex.text;
+        tex.text = "";
+        i = 15;
+    }
+
+    void Update()
+    {
+        if (ison)
+        {
+            i -= 1;
+            if (i <= 0)
+            {
+                if(index >= str.Length)
+                {
+                    ison = false;
+                    return;
+                }
+                str1 = str1 + str[index].ToString();
+                tex.text = str1;
+                index += 1;
+                i = 15;
+            }
+        }
+    }
+}
